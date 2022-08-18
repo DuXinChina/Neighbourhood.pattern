7a9660b63b082b3d6769b035f4dfd38ebfc8fa66
DuXinChina <71441722+DuXinChina@users.noreply.github.com>
2022-08-18 21:00:42 +0800

    Add files via upload

---
+++ Neighbourhood.pattern/R/MI.Tan.S.R
@@ -0,0 +1,178 @@
+####计算加权进界邻体荫蔽度的修正系数function~
+MI.Tan.S=function(minx,maxx,miny,maxy,b,seq,tan,MI)
+{
+  library(sp)
+  library(gstat)
+  library(tcltk)
+  #################
+  ####生成插值点
+  xgrid=seq(minx,maxx, length.out = seq+1)
+  ygrid=seq(miny,maxy, length.out = seq+1)
+  basexy=expand.grid(xgrid, ygrid)
+  xgrid.cen=seq(minx+0.5*(maxx-minx)-0.5/seq*(maxx-minx),minx+0.5*(maxx-minx)+0.5/seq*(maxx-minx), length.out = 3)
+  ygrid.cen=seq(miny+0.5*(maxy-miny)-0.5/seq*(maxy-miny),miny+0.5*(maxy-miny)+0.5/seq*(maxy-miny), length.out = 3)
+  basexy.cen=expand.grid(xgrid.cen, ygrid.cen)
+  
+  xgrid.left.top=seq(minx+0.25*(maxx-minx)-0.5/seq*(maxx-minx),minx+0.25*(maxx-minx)+0.5/seq*(maxx-minx), length.out = 3)
+  ygrid.left.top=seq(miny+0.75*(maxy-miny)-0.5/seq*(maxy-miny),miny+0.75*(maxy-miny)+0.5/seq*(maxy-miny), length.out = 3)
+  basexy.left.top=expand.grid(xgrid.left.top, ygrid.left.top)
+  
+  xgrid.left.bottom=seq(minx+0.25*(maxx-minx)-0.5/seq*(maxx-minx),minx+0.25*(maxx-minx)+0.5/seq*(maxx-minx), length.out = 3)
+  ygrid.left.bottom=seq(miny+0.25*(maxy-miny)-0.5/seq*(maxy-miny),miny+0.25*(maxy-miny)+0.5/seq*(maxy-miny), length.out = 3)
+  basexy.left.bottom=expand.grid(xgrid.left.bottom, ygrid.left.bottom)
+  
+  xgrid.right.top=seq(minx+0.75*(maxx-minx)-0.5/seq*(maxx-minx),minx+0.75*(maxx-minx)+0.5/seq*(maxx-minx), length.out = 3)
+  ygrid.right.top=seq(miny+0.75*(maxy-miny)-0.5/seq*(maxy-miny),miny+0.75*(maxy-miny)+0.5/seq*(maxy-miny), length.out = 3)
+  basexy.right.top=expand.grid(xgrid.right.top, ygrid.right.top)
+  
+  xgrid.right.bottom=seq(minx+0.75*(maxx-minx)-0.5/seq*(maxx-minx),minx+0.75*(maxx-minx)+0.5/seq*(maxx-minx), length.out = 3)
+  ygrid.right.bottom=seq(miny+0.25*(maxy-miny)-0.5/seq*(maxy-miny),miny+0.25*(maxy-miny)+0.5/seq*(maxy-miny), length.out = 3)
+  basexy.right.bottom=expand.grid(xgrid.right.bottom, ygrid.right.bottom)
+  #basexy1=basexy
+  xgrid=seq(minx,maxx, length.out = 200)
+  ygrid=seq(miny,maxy, length.out = 200)
+  basexy1=expand.grid(xgrid, ygrid)
+  Basexy1=basexy1
+  basexy=rbind(basexy,basexy.cen,basexy.left.top,basexy.left.bottom,basexy.right.top,basexy.right.bottom)
+  colnames(basexy1) <- c("x", "y")
+  coordinates(basexy1) <- ~x+y
+  gridded(basexy1) <- TRUE
+  basexy[,3]=0
+  colnames(basexy) <- c("x", "y","Size")
+  a= dplyr::distinct(basexy)
+  ####删除重复样点
+  ################
+  Tan.dependence.S.mult=function(a,b,tan)
+  {
+    library(tcltk)
+    Tan.dependence.Wei_S.single=function(a,b,tan)
+    {
+      
+      Tan.dependence.S.single=function(a,b,tan)
+      {
+        Neighbourhood.single1=function(a,b,tan)
+          ###找出一定尺度内的邻体
+        {#3
+          c=b[,1:2]
+          for (i in 1:nrow(b))
+          {#4
+            c[i,]=(b[i,1:2]-a[1,1:2])^2
+            d=(c[,1]+c[,2])^(1/2)
+          }#4
+          d=cbind(b,d)
+          #d=subset(d,d>0)
+          d$tangent=(d$size-a[,3])/d$d
+          d=subset(d,tangent>tan)
+          colnames(d) = c("x","Y","Size","Distance","tangent")
+          d
+        }
+        Nei.tree=Neighbourhood.single1(a,b,tan)
+        n.dif=nrow(Nei.tree)
+        if(n.dif==0)
+        {Tan_dependence_S=0
+        Neighbourhood=Nei.tree}
+        if(n.dif!=0)
+        {
+          Neighbourhood=Nei.tree
+          Neighbourhood1=Neighbourhood
+          Neighbourhood[which(Neighbourhood[,5]<=0),5]=NA
+          Neighbourhood1[which(Neighbourhood1[,5]<=0),5]=0
+          Neighbourhood1[which(Neighbourhood1[,4]==0),4]=0.00000001
+          S1=Neighbourhood1[,5]/Neighbourhood1[,4]
+          S2=as.data.frame(S1)
+          S=sum(S2,na.rm=T)
+          Tan_dependence_S=S
+        }
+        
+        if(is.nan(Tan_dependence_S)==T)(Tan_dependence_S=100000000)
+        outcome=list(a=a,Neighbourhood=Neighbourhood,Tan_dependence_S=Tan_dependence_S)
+        outcome
+      }
+      
+      Tan.dependence.Simpson=function(a,b,tan)
+      {
+        Neighbourhood.single1=function(a,b,tan)
+          ###找出一定尺度内的邻体         
+        {#3
+          c=b[,1:2]
+          for (i in 1:nrow(b))
+          {#4
+            c[i,]=(b[i,1:2]-a[1,1:2])^2
+            d=(c[,1]+c[,2])^(1/2)
+          }#4 
+          d=cbind(b,d)
+          #d=subset(d,d>0)
+          d$tangent=(d$size-a[,3])/d$d
+          d=subset(d,tangent>tan)
+          colnames(d) = c("x","y","Size","Distance","tangent")
+          d
+        }
+        Nei.tree=Neighbourhood.single1(a,b,tan)
+        Nei.tree[,3]=Nei.tree[,3]-a[,3]
+        Nei.tree=subset(Nei.tree,Size>0)
+        Nei.tree
+        Simpn=sum(Nei.tree[,3])
+        Nei.tree=subset(Nei.tree,Distance>0)
+        Nei.tree1=subset(Nei.tree,x>=a[,1]&y>a[,2])
+        ####将Nei.tree划分到四个象限
+        Nei.tree2=subset(Nei.tree,x>a[,1]&y<=a[,2])
+        Nei.tree3=subset(Nei.tree,x<=a[,1]&y<a[,2])
+        Nei.tree4=subset(Nei.tree,x<a[,1]&y>=a[,2])
+        Simpn1=sum(Nei.tree1[,3])
+        Simpn2=sum(Nei.tree2[,3])
+        Simpn3=sum(Nei.tree3[,3])
+        Simpn4=sum(Nei.tree4[,3])
+        simp_wei=sum((Simpn1/Simpn)^2,(Simpn2/Simpn)^2,(Simpn3/Simpn)^2,(Simpn4/Simpn)^2)
+        if(is.nan(simp_wei)==T)(simp_wei=0.25)
+        simp_wei
+      }
+      #####计算尺度依赖加权荫蔽度
+      S=Tan.dependence.S.single(a,b,tan)
+      Levins_Simpson=Tan.dependence.Simpson(a,b,tan)
+      
+      
+      #####计算尺度依赖加权荫蔽度
+      S=Tan.dependence.S.single(a,b,tan)
+      a=S$a
+      Neighbourhood=S$Neighbourhood
+      Tan_dependence_S=S$Tan_dependence_S
+      
+      Tan_dependence_Wei_S=Tan_dependence_S*(0.25/Levins_Simpson)
+      outcome=list(a=a,Neighbourhood=Neighbourhood,Tan_dependence_S=Tan_dependence_S,Levins_Simpson=Levins_Simpson,Tan_dependence_Wei_S=Tan_dependence_Wei_S)
+      outcome
+    }
+    d=matrix(NA,nrow(a),3)
+    pb=tkProgressBar("Progress","Percent complete %", 0, 100)
+    star_time=Sys.time() 
+    ## 记录程序开始时间
+    for(j in 1:nrow(a))
+    {
+      d[j,]=cbind(as.matrix(a[j,1:2]),as.matrix(Tan.dependence.Wei_S.single(a[j,],b,tan)$Tan_dependence_Wei_S))
+      info=sprintf("Percent complete %d%%", round(j*100/nrow(a))) 
+      ## 设置进度条的完成度
+      setTkProgressBar(pb, j*100/nrow(a), sprintf("Progress (%s)", info),info)
+      ## 设置进度条
+    }
+    end_time =Sys.time() 
+    ## 记录程序结束时间
+    close(pb)
+    run_time = end_time - star_time 
+    ## 计算程序运行时间
+    colnames(d)=c("x","y","Tan_dependence_Wei_S")
+    rownames(d)=1:nrow(a)
+    d=as.data.frame(d)
+    d
+  }
+  data=Tan.dependence.S.mult(a,b,tan)
+  data=subset(data,data[,3]>0)
+  data=data[,3]
+  n_=length(data)
+  x=matrix()
+  data=data[order(data)]
+  for(i in 1:n_){x[i]=(atan(data[i]/(MI*pi))/pi*2)}
+  data=1:n_
+  lm=lm(data~x)
+  print(plot(x,data)+abline(lm))
+  print
+  (summary(lm))
+}
