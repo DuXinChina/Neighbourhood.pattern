1167c16c8e74734ca8a0dedfd2b6abcd45ba7ceb
DuXinChina <71441722+DuXinChina@users.noreply.github.com>
2023-03-09 11:57:00 +0800

    Update plot.Neighbourhood.pattern.S.dif_sp.R

--- Neighbourhood.pattern/R/plot.Neighbourhood.pattern.S.dif_sp.R
+++ Neighbourhood.pattern/R/plot.Neighbourhood.pattern.S.dif_sp.R
@@ -43,15 +43,15 @@ plot.Neighbourhood.pattern.S.dif_sp=function (a, b, n)
       outcome
     }
     d = matrix(NA, nrow(a), 4)
-    pb = tkProgressBar("½ø¶È", "ÒÑÍê³É %", 
+    pb = tkProgressBar("è¿›åº¦", "å·²å®Œæˆ %", 
                        0, 100)
     star_time = Sys.time()
     for (j in 1:nrow(a)) {
       d[j, ] = cbind(as.matrix(a[j, ]), as.matrix(Neighbourhood.pattern.S.single(a[j, 
       ], b, n)$Neighbourhood_pattern_S))
-      info = sprintf("ÒÑÍê³É %d%%", round(j * 
+      info = sprintf("å·²å®Œæˆ %d%%", round(j * 
                                          100/nrow(a)))
-      setTkProgressBar(pb, j * 100/nrow(a), sprintf("½ø¶È (%s)", 
+      setTkProgressBar(pb, j * 100/nrow(a), sprintf("è¿›åº¦ (%s)", 
                                                     info), info)
     }
     end_time = Sys.time()
