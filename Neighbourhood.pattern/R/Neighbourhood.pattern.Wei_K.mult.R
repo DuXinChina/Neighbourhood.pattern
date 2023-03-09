ee1d2eb17dea3d8d8225423060b401642b39683e
DuXinChina <71441722+DuXinChina@users.noreply.github.com>
2023-03-09 10:56:17 +0800

    Update Neighbourhood.pattern.Wei_K.mult.R

--- Neighbourhood.pattern/R/Neighbourhood.pattern.Wei_K.mult.R
+++ Neighbourhood.pattern/R/Neighbourhood.pattern.Wei_K.mult.R
@@ -105,14 +105,14 @@ Neighbourhood.pattern.Wei_K.mult=function (a, b, n)
     outcome
   }
   d = matrix(NA, nrow(a), 3)
-  pb = tkProgressBar("½ø¶È", "ÒÑÍê³É %", 0, 
+  pb = tkProgressBar("è¿›åº¦", "å·²å®Œæˆ %", 0, 
                      100)
   star_time = Sys.time()
   for (j in 1:nrow(a)) {
     d[j, ] = cbind(as.matrix(a[j, 1:2]), as.matrix(Neighbourhood.pattern.Wei_K.single(a[j, 
     ], b, n)$Neighbourhood_pattern_Wei_K))
-    info = sprintf("ÒÑÍê³É %d%%", round(j * 100/nrow(a)))
-    setTkProgressBar(pb, j * 100/nrow(a), sprintf("½ø¶È (%s)", 
+    info = sprintf("å·²å®Œæˆ %d%%", round(j * 100/nrow(a)))
+    setTkProgressBar(pb, j * 100/nrow(a), sprintf("è¿›åº¦ (%s)", 
                                                   info), info)
   }
   end_time = Sys.time()
