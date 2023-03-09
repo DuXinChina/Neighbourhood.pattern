bbc23bcb379ce65979cc34a0e6959734d0ac510d
DuXinChina <71441722+DuXinChina@users.noreply.github.com>
2023-03-09 11:01:44 +0800

    Update plot.Neighbourhood.pattern.C.mult.R

--- Neighbourhood.pattern/R/plot.Neighbourhood.pattern.C.mult.R
+++ Neighbourhood.pattern/R/plot.Neighbourhood.pattern.C.mult.R
@@ -30,15 +30,15 @@ plot.Neighbourhood.pattern.C.mult=function (a, b, n)
       outcome
     }
     d = matrix(NA, nrow(a), 3)
-    pb = tkProgressBar("½ø¶È", "ÒÑÍê³É %", 
+    pb = tkProgressBar("è¿›åº¦", "å·²å®Œæˆ %", 
                        0, 100)
     star_time = Sys.time()
     for (j in 1:nrow(a)) {
       d[j, ] = cbind(as.matrix(a[j, 1:2]), as.matrix(Neighbourhood.pattern.C.single(a[j, 
       ], b, n)$C))
-      info = sprintf("ÒÑÍê³É %d%%", round(j * 
+      info = sprintf("å·²å®Œæˆ %d%%", round(j * 
                                          100/nrow(a)))
-      setTkProgressBar(pb, j * 100/nrow(a), sprintf("½ø¶È (%s)", 
+      setTkProgressBar(pb, j * 100/nrow(a), sprintf("è¿›åº¦ (%s)", 
                                                     info), info)
     }
     end_time = Sys.time()
